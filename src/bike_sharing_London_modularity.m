357429e1518c24a4d59757b1622de76ed2eaf55d
colesturza <colesturza@gmail.com>
2020-12-13 00:31:43 -0500

    modularity tests

--- src/bike_sharing_London_modularity.m
+++ src/bike_sharing_London_modularity.m
@@ -1,7 +1,10 @@
-filename = './bikesharing/mod_divide_dist_squared.csv';
+filename = './bikesharing/mod_simple_bin_10.csv';
+filename1 = strcat(filename, '.csv');
 B = csvread(filename);
 
 [S,Q] = genlouvain(B);
 
-filename = './bikesharing/mod_divide_dist_squared_communities.csv';
+Q
+
+filename = strcat(filename, '_communities.csv');
 csvwrite(filename, S);
